740773b2f146f977968c35edada0b7fe0dc0f1b1
Neil Hiranandani <neil@neilbefore.me>
2023-05-19 14:13:43 -0400

    fixed algo setup

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as skyscraper from './skyscraper'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [skyscraper.shortname]: skyscraper.handler,
 }
 
 export default algos
